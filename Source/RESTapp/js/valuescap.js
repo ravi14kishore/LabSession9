0546bb02260a06d51c1b62ff5f70e935d8bcafec
ravi14kishore <slxfc@mail.umkc.edu>
2016-03-24 04:57:34 -0500

    commit

---
+++ Source/RESTapp/js/valuescap.js
@@ -0,0 +1,13 @@
+var myapp = angular.module('starter', []);
+
+myapp.comtroller('myAppController', function($scope)
+{
+    $scope.username = function()
+    {
+        return "akhi";
+    }
+    $scope.password = function()
+    {
+        return "akhi";
+    }
+})
